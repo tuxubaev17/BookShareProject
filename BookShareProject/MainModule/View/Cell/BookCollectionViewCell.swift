2c9c2a9e0e7d8ad19378ebbc5788514f271aa4ad
tuxubaev17 <tuxubaev17@mail.com>
2021-03-11 14:20:47 +0500

    modifie viewModel

---
+++ BookShareProject/MainModule/View/Cell/BookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BookCollectionViewCell.swift
+//  BookShareProject
+//
+//  Created by Alikhan Tuxubayev on 09.03.2021.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+}
